099115ed92b086184e0c46eb945a9e7e4d59b652
Stephen Chibundu Onyeje <98972158+chibunduonyeje1@users.noreply.github.com>
2023-01-07 22:19:30 +0100

    Update validator.js

--- assets/js/validator.js
+++ assets/js/validator.js
@@ -1,20 +1,20 @@
-const nameField = document.getElementById('fullname');
-const emailField = document.getElementById('email-field');
-const messageField = document.getElementById('message-field');
-const submitButton = document.getElementById('submit');
+const nameField = document.getElementById('.fullname');
+const emailField = document.getElementById('.email-field');
+const messageField = document.getElementById('.message-field');
+const submitButton = document.getElementById('.submit');
 
-const nameErrorMsg = document.getElementById('name-error');
-const emailErrorMsg = document.getElementById('email-error');
-const messageError = document.getElementById('message-error');
+const nameErrorMsg = document.getElementById('.name-error');
+const emailErrorMsg = document.getElementById('.email-error');
+const messageError = document.getElementById('.message-error');
 
 submitButton.addEventListener('click', () => {
-  if (nameField.value.length === 0) {
+  if (nameField.value.length = 0) {
     nameErrorMsg.innerHTML = 'Your Name is required';
   } else {
     nameErrorMsg.innerHTML = '';
   }
 
-  if (emailField.value.length === 0) {
+  if (emailField.value.length = 0) {
     emailErrorMsg.innerHTML = 'Your Email is required';
   } else if (emailField.value !== emailField.value.toLowerCase()) {
     emailErrorMsg.innerHTML = 'Please input your email address in lowercase.';
@@ -22,7 +22,7 @@ submitButton.addEventListener('click', () => {
     emailErrorMsg.innerHTML = '';
   }
 
-  if (messageField.value.length === 0) {
+  if (messageField.value.length = 0) {
     messageError.innerHTML = 'Your message field is empty';
   } else {
     messageError.innerHTML = '';
